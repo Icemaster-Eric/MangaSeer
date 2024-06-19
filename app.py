5ad95ade8d4dadf8336542e65a6721366342d083
Eric Li <icemastereric@gmail.com>
2024-06-19 17:52:35 -0600

    got mouse events working

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from PySide6 import QtWidgets, QtCore
 
 
 class Popup(QtWidgets.QWidget):
-    def __init__(self, parent: QtWidgets.QWidget = None):
+    def __init__(self, bbox: tuple[int, int, int, int], parent: QtWidgets.QWidget):
         super().__init__(parent)
 
         layout = QtWidgets.QHBoxLayout()
@@ -11,25 +11,28 @@ class Popup(QtWidgets.QWidget):
 
         self.setLayout(layout)
 
-        self.setStyleSheet("QWidget { background-color : green}") # debugging
-        self.setGeometry(100, 100, self.sizeHint().width(), self.sizeHint().height())
-        self.setFixedSize(100, 100)
-
+        self.setStyleSheet("QWidget { border : 3px solid red}")
+        #self.setGeometry(0, 0, self.sizeHint().width(), self.sizeHint().height())
+        self.setGeometry(*bbox)
         self.show()
 
+    def mousePressEvent(self, event):
+        print(type(event))
 
-class Overlay(QtWidgets.QWidget):
+
+class Overlay(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-
-        # NOTE: choose a better name than "popup"
-        self.popups = [
-            Popup(self)
-        ]
 
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground, True)
         self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint | QtCore.Qt.WindowType.WindowStaysOnTopHint)
 
+        # NOTE: choose a better name than "popup"
+        self.popups = [
+            Popup((100, 100, 200, 200), self)
+        ]
+
+        self.move(0, 0)
         self.showMaximized()
 
 
