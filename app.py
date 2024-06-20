91e7eb3a9f02087efad1e87f10abfeae98e19e3a
Eric Li <icemastereric@gmail.com>
2024-06-20 12:23:54 -0600

    switched to dxcam for screenshotting

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from PySide6 import QtWidgets, QtCore
+from PySide6 import QtWidgets, QtCore, QtGui
 from ultralytics import YOLOv10
-from PIL import ImageGrab
+from PIL import Image
+import dxcam
+import time
 
 
 class Popup(QtWidgets.QWidget):
@@ -13,7 +15,7 @@ class Popup(QtWidgets.QWidget):
 
         self.setLayout(layout)
 
-        self.setStyleSheet("QWidget { border : 3px solid red}")
+        self.setStyleSheet("QWidget { border: 3px solid red }")
         #self.setGeometry(0, 0, self.sizeHint().width(), self.sizeHint().height())
         self.setGeometry(
             bbox[0] - bbox[2] / 2,
@@ -32,6 +34,7 @@ class Overlay(QtWidgets.QMainWindow):
         super().__init__()
 
         self.model = YOLOv10("models/2.pt")
+        self.camera = dxcam.create()
         self.popups: list[Popup] = []
 
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground, True)
@@ -41,22 +44,27 @@ class Overlay(QtWidgets.QMainWindow):
         self.showMaximized()
 
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(3000)
+        self.timer.setInterval(1000)
         self.timer.timeout.connect(self.scan_screen)
         self.timer.start()
 
     def scan_screen(self):
-        # rename this function because the name sounds kinda stupid rn
+        # rename this function
         for popup in self.popups:
+            popup.hide()
+
+        app.processEvents()
+
+        start = time.time()
+        snapshot = Image.fromarray(self.camera.grab())
+        print(time.time() - start)
+
+        for popup in self.popups:
+            popup.show()
             popup.deleteLater()
+
         self.popups.clear()
-
-        self.hide()
-
-        snapshot = ImageGrab.grab()
-        snapshot.save("img.png")
-
-        self.showMaximized()
+        app.processEvents()
 
         bboxes = self.model(source=snapshot, conf=0.25)[0].boxes
 
