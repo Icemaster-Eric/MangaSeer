fd17fea688d21570ba3f729c801f961fd1a7c7b4
Eric Li <icemastereric@gmail.com>
2024-06-24 17:03:50 -0600

    fully cleaned up dataset, removed useless code functions, laid groundwork for furigana generation

--- dataset.py
+++ dataset.py
@@ -3,17 +3,17 @@ import random
 from PIL import Image, ImageFont, ImageDraw
 import ujson
 from tqdm import tqdm
-import re
+import pykakasi
 
 
-def get_conv_dataset():
-    conv_dataset = []
+def get_conv_dataset() -> set:
+    conv_dataset = set()
 
     for file_name in listdir("datasets/japanese/conv-json"):
         with open(f"datasets/japanese/conv-json/{file_name}", "r", encoding="utf-8") as f:
             for dialogue in ujson.load(f):
                 for utterance in dialogue["utterances"]:
-                    conv_dataset.append(utterance["utterance"])
+                    conv_dataset.add(utterance["utterance"])
 
     return conv_dataset
 
@@ -51,52 +51,19 @@ def get_news_dataset() -> set:
     return set(sentence.strip() for sentence in sentences)
 
 
-def generate_synthetic_dataset(kanji: tuple) -> set:
-    hiragana = [chr(i) for i in range(int("3041", 16), int("3097", 16))]
-    katakana = [chr(i) for i in range(int("30A1", 16), int("30FF", 16))]
-    hiragana = "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわをんゔゕゖ"
-    katakana = "ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヲンヴヵヶヺ"
-    choices = (
-        "...",
-        ".....",
-        "ー",
-        *hiragana,
-        *katakana,
-        *kanji
-    )
-    weights = (
-        50,
-        50,
-        50,
-        *([1500] * len(hiragana)),
-        *([500] * len(katakana)),
-        *([30] * len(kanji))
-    )
-    return set(tqdm(
-        ("".join(random.choices(choices, weights, k=random.randint(1, 30))) for _ in range(40000)),
-        total=40000
-    ))
-
-
-def get_synth_dataset() -> set:
-    with open("datasets/japanese/synth_dataset.txt", "r", encoding="utf-8") as f:
-        dataset = f.readlines()
-
-    return set(sentence.strip() for sentence in dataset)
-
-
 def main():
     conv_dataset = get_conv_dataset()
+    #news_dataset = get_news_dataset()
+    #dataset = conv_dataset.union(news_dataset)
     kanji_set = tuple(get_kanji())
-    #synth_dataset = get_synth_dataset()
+
+    kks = pykakasi.kakasi()
+    text = conv_dataset.pop()
+    result = kks.convert(text)
+    for item in result:
+        print("{}[{}] ".format(item["orig"], item["kana"].capitalize()), end="")
+    print()
 
 
 if __name__ == "__main__":
-    #main()
-    with open("datasets/japanese/news_dataset.txt", "r", encoding="utf-8") as f:
-        dataset = f.readlines()
-
-    with open("datasets/japanese/news_dataset_clean.txt", "a", encoding="utf-8") as f:
-        for sentence in tqdm(dataset):
-            if not re.search("[a-z]", sentence.lower()):
-                f.write(sentence)
+    main()
