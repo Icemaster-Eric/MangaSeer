f9be28174ba95914386ff4ec8c9ea2f02bd07adf
Eric Li <icemastereric@gmail.com>
2024-06-24 22:27:38 -0600

    use html2image library instead of QtWebEngine for rendering html

--- utils/html_render.py
+++ utils/html_render.py
@@ -1,36 +1,31 @@
-from PySide6 import QtWidgets, QtCore, QtGui, QtWebEngineWidgets
+from html2image import Html2Image
 
 
-class Render(QtWebEngineWidgets.QWebEngineView):
-    def __init__(self, html_to_render: set[str]):
-        super().__init__()
+hti = Html2Image("edge", size=(500, 500), custom_flags=["--no-sandbox"])
 
-        self.current_html = None
-        self.html_to_render = html_to_render
+css_str = """
+@font-face {
+    font-family: "GenEiPOP";
+    src: url("E:/Code/MangaSeer/datasets/fonts/GenEiPOPlePw-Bk.ttf");
+}
+body {
+    background-color: white;
+    font-size: 32px;
+}
+p {
+    font-family: "GenEiPOP";
+    writing-mode: vertical-rl;
+    text-orientation: upright;
+}
+"""
 
-        #self.loadFinished.connect(self._loadFinished)
-
-        self.setHtml(self.html_to_render.pop())
-        self.printFinished.connect(self._loadFinished)
-
-        while len(self.html_to_render):
-            while not self.current_html:
-                app.processEvents(QtCore.QEventLoop.ProcessEventsFlag.ExcludeUserInputEvents | QtCore.QEventLoop.ProcessEventsFlag.ExcludeSocketNotifiers | QtCore.QEventLoop.ProcessEventsFlag.WaitForMoreEvents)
-
-            self.screenshot()
-            print("ss!!!")
-            self.current_html = None
-
-        app.quit()
-
-    def _loadFinished(self, data):
-        self.current_html = data
-
-    def screenshot(self):
-        self.grab().save(f"ss.png", "png")
+hti.screenshot(html_str="<p><ruby>具合<rt>ぐあい</rt></ruby>は、どうで<ruby>具合<rt>ぐあい</rt></ruby>すか?</p>", css_str=css_str, save_as="ss.png")
 
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication()
-    render = Render([f"<h1>{i} some text here idk {i}" for i in range(50)])
-    app.exec()
+class Renderer:
+    def __init__(self):
+        self.hti = Html2Image("edge", size=(500, 500), custom_flags=["--no-sandbox"])
+
+    def render(html):
+        # calculate bounding box of rendered html and crop it using Pillow as postprocessing step (`getbbox` function)
+        pass
