f8856faf9023c7a3aa22e03ab728ec170157fc91
Eric Li <icemastereric@gmail.com>
2024-07-09 19:13:30 -0600

    replaced scroll area with custom flow layout for wrapping

--- app.py
+++ app.py
@@ -4,6 +4,96 @@ from PySide6 import QtWidgets, QtCore, QtGui
 from utils import screenshot, tts, JMDict
 
 
+class FlowLayout(QtWidgets.QLayout):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        if parent is not None:
+            self.setContentsMargins(QtCore.QMargins(0, 0, 0, 0))
+
+        self._item_list = []
+
+    def __del__(self):
+        item = self.takeAt(0)
+        while item:
+            item = self.takeAt(0)
+
+    def addItem(self, item):
+        self._item_list.append(item)
+
+    def count(self):
+        return len(self._item_list)
+
+    def itemAt(self, index):
+        if 0 <= index < len(self._item_list):
+            return self._item_list[index]
+
+        return None
+
+    def takeAt(self, index):
+        if 0 <= index < len(self._item_list):
+            return self._item_list.pop(index)
+
+        return None
+
+    def expandingDirections(self):
+        return QtCore.Qt.Orientation(0)
+
+    def hasHeightForWidth(self):
+        return True
+
+    def heightForWidth(self, width):
+        height = self._do_layout(QtCore.QRect(0, 0, width, 0), True)
+        return height
+
+    def setGeometry(self, rect):
+        super(FlowLayout, self).setGeometry(rect)
+        self._do_layout(rect, False)
+
+    def sizeHint(self):
+        return self.minimumSize()
+
+    def minimumSize(self):
+        size = QtCore.QSize()
+
+        for item in self._item_list:
+            size = size.expandedTo(item.minimumSize())
+
+        size += QtCore.QSize(2 * self.contentsMargins().top(), 2 * self.contentsMargins().top())
+        return size
+
+    def _do_layout(self, rect, test_only):
+        x = rect.x()
+        y = rect.y()
+        line_height = 0
+        spacing = self.spacing()
+
+        for item in self._item_list:
+            style = item.widget().style()
+            layout_spacing_x = style.layoutSpacing(
+                QtWidgets.QSizePolicy.PushButton, QtWidgets.QSizePolicy.PushButton, QtCore.Qt.Horizontal
+            )
+            layout_spacing_y = style.layoutSpacing(
+                QtWidgets.QSizePolicy.ControlType.PushButton, QtWidgets.QSizePolicy.ControlType.PushButton, QtCore.Qt.Orientation.Vertical
+            )
+            space_x = spacing + layout_spacing_x
+            space_y = spacing + layout_spacing_y
+            next_x = x + item.sizeHint().width() + space_x
+            if next_x - space_x > rect.right() and line_height > 0:
+                x = rect.x()
+                y = y + line_height + space_y
+                next_x = x + item.sizeHint().width() + space_x
+                line_height = 0
+
+            if not test_only:
+                item.setGeometry(QtCore.QRect(QtCore.QPoint(x, y), item.sizeHint()))
+
+            x = next_x
+            line_height = max(line_height, item.sizeHint().height())
+
+        return y + line_height - rect.y()
+
+
 class TextEdit(QtWidgets.QInputDialog):
     def __init__(self, parent: QtWidgets.QWidget, text: str):
         super().__init__(parent)
@@ -23,9 +113,19 @@ class PopupButton(QtWidgets.QPushButton):
         self.setCursor(QtGui.Qt.CursorShape.PointingHandCursor)
 
 
-class WordInfo(QtWidgets.QLabel):
+class WordInfo(QtWidgets.QWidget):
     def __init__(self, word: dict):
-        super().__init__(str(word))
+        super().__init__()
+
+        layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+
+        layout.addWidget(QtWidgets.QLabel("testing"))
+
+        if "reading" in word:
+            reading_label = QtWidgets.QLabel(word["reading"])
+            reading_label.setStyleSheet("QLabel { font-family: 'Noto Sans JP'; font-size: 18px; border-radius: 5px; }")
+            layout.addWidget(reading_label)
 
 
 class DictWord(QtWidgets.QLabel):
@@ -35,8 +135,7 @@ class DictWord(QtWidgets.QLabel):
         self.word = word
         self.info_widget = info_widget
 
-        self.setStyleSheet("QLabel { font-size: 20px; border-radius: 5px; } QLabel:hover { background-color: rgba(0, 191, 255, 0.5); }")
-        self.setFont("Noto Sans JP")
+        self.setStyleSheet("QLabel { font-family: 'Noto Sans JP'; font-size: 20px; border-radius: 5px; } QLabel:hover { background-color: rgba(0, 191, 255, 0.5); }")
         self.setCursor(QtGui.Qt.CursorShape.PointingHandCursor)
 
     def mousePressEvent(self, event):
@@ -58,39 +157,28 @@ class Dictionary(QtWidgets.QDialog):
         words = jmdict.lookup(text, common=True)
 
         self.setWindowTitle("Dictionary")
-        self.setMaximumWidth(600)
 
         layout = QtWidgets.QVBoxLayout()
         self.setLayout(layout)
 
-        scroll_area = QtWidgets.QScrollArea()
-
-        container_widget = QtWidgets.QWidget()
-        container_layout = QtWidgets.QVBoxLayout()
-        container_widget.setLayout(container_layout)
-
         info_widget = QtWidgets.QListWidget()
 
         text_widget = QtWidgets.QWidget()
-        text_layout = QtWidgets.QHBoxLayout()
+        text_layout = FlowLayout()
         text_widget.setLayout(text_layout)
         text_layout.setSpacing(0)
 
         for word in words:
             if not word["type"]:
                 text_label = QtWidgets.QLabel(word["text"])
-                text_label.setStyleSheet("QLabel { font-size: 20px; }")
-                text_label.setFont("Noto Sans JP")
+                text_label.setStyleSheet("QLabel { font-family: 'Noto Sans JP'; font-size: 20px; }")
             else:
                 text_label = DictWord(word, info_widget)
 
             text_layout.addWidget(text_label)
 
-        container_layout.addWidget(text_widget)
-        container_layout.addWidget(info_widget)
-
-        scroll_area.setWidget(container_widget)
-        layout.addWidget(scroll_area)
+        layout.addWidget(text_widget)
+        layout.addWidget(info_widget)
 
 
 class Popup(QtWidgets.QWidget):
